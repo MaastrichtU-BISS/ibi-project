1156ff5b4024022371104e8856c6d1664a10371d
sabaturgay <turgaysaba@outlook.com>
2021-08-02 11:48:51 +0300

    Fix HalfPieChart file name

--- components/ChartExamples/index.tsx
+++ components/ChartExamples/index.tsx
@@ -1,4 +1,4 @@
 export { default as PieChart } from './PieChart'
 export { default as RadarChart } from './RadarChart'
 export { default as BarChart } from './BarChart'
-export { default as HalfPieChart } from './HalFPieChart'
+export { default as HalfPieChart } from './HalfPieChart'
